a5c761fb3008a470c11b403570cfebaceb01ce6a
Zhuryd Andrei <zhuryd.andrei@gmail.com>
2023-04-17 11:47:15 +0300

    17/04/2023

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import './Button.scss'
 
-export const Button = () => {
+export const AddButton = props => {
   return (
     <div className="buttons">
-      <button className="button">
-        Добавить
+      <button className="button addBtn">
+        {props.children}
       </button>
     </div>
   )
 }
 
-export const logger = () => {
-  console.log('LOGGER')
+export const RemoveButton = props => {
+  return (
+    <div className="main__card-buttons">
+      <button className="main__card-button button removeBtn">
+        {props.children}
+      </button>
+    </div>
+  )
 }
-
-// export default Button
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './Main.scss'
 
 import Input from "./Inputs/Input"
-import {Button} from "../Buttons/Button"
+import {AddButton} from "../Buttons/Button"
+import Card from "./Card/Card"
+
 
 const Main = () => {
   return (
@@ -11,21 +13,12 @@ const Main = () => {
 
         <Input />
 
-
-        <Button/>
+        <AddButton>
+          Добавить
+        </AddButton>
 
       </div>
-      <div className="main__cards">
-        <div className="main__card">
-          <div className="main__card-content">
-            <span className="main__card-title">##TITLTE##</span>
-            <span className="main__card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, odit.</span>
-          </div>
-          <div className="main__card-buttons">
-            <button className="main__card-button">Delete</button>
-          </div>
-        </div>
-      </div>
+        <Card />
     </div>
   )
 }
